36240ca34788ab7445b3958025d09954ad79a779
andrewhamili <andrewhamilidevelopment@gmail.com>
2025-07-07 11:44:44 +0800

    scaffold media module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MediaModule } from './media/media.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MediaModule],
   controllers: [AppController],
   providers: [AppService],
 })
